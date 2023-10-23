a0c801e461af2c94b06fb96aa772966004e09d4a
burgokmen <burgokmen@gmail.com>
2023-10-23 14:39:00 +0300

    init

---
+++ spring-jpa-project/src/test/java/com/brutech/springjpaproject/SpringJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brutech.springjpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
