c585bc67d61906b0ca5a00db75b92c8235353ed8
burgokmen <burgokmen@gmail.com>
2023-10-23 20:29:49 +0300

    Burger implementation

--- spring-jpa-project/src/main/java/com/brutech/springjpaproject/dao/BurgerDaoImpl.java
+++ spring-jpa-project/src/main/java/com/brutech/springjpaproject/dao/BurgerDaoImpl.java
@@ -3,6 +3,7 @@ package com.brutech.springjpaproject.dao;
 import com.brutech.springjpaproject.entity.Burger;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -14,15 +15,16 @@ public class BurgerDaoImpl implements BurgerDao {
     public BurgerDaoImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-
+    @Transactional
     @Override
     public Burger save(Burger burger) {
-        return null;
+        entityManager.persist(burger);
+        return burger;
     }
 
     @Override
     public Burger findById(Long id) {
-        return null;
+        return entityManager.find(Burger.class, id);
     }
 
     @Override
@@ -35,31 +37,38 @@ public class BurgerDaoImpl implements BurgerDao {
     @Override
     public List<Burger> findByPrice(Double price) {
         TypedQuery<Burger> query = entityManager
-                .createQuery("SELECT b FROM Burger b WHERE b.price = :price", Burger.class);
+                .createQuery("SELECT b FROM Burger b WHERE price > :price ORDER BY b.price DESC", Burger.class);
+        query.setParameter("price", price);
         return query.getResultList();
     }
 
     @Override
     public List<Burger> findByBreadType(String breadType) {
         TypedQuery<Burger> query = entityManager
-                .createQuery("SELECT b FROM Burger b WHERE b.breadType = :breadType", Burger.class);
+                .createQuery("SELECT b FROM Burger b WHERE b.breadType = :breadType ORDER BY b.name", Burger.class);
+        query.setParameter("breadType", breadType);
         return query.getResultList();
     }
 
     @Override
     public List<Burger> findByContent(String content) {
         TypedQuery<Burger> query = entityManager
-                .createQuery("SELECT b FROM Burger b WHERE b.contents = :content", Burger.class);
+                .createQuery("SELECT b FROM Burger b WHERE b.contents LIKE CONCAT('%' , :content , '%') ORDER BY b.name", Burger.class);
+        query.setParameter("content", content);
         return query.getResultList();
     }
-
+    @Transactional
     @Override
     public Burger update(Burger burger) {
-        return null;
+        return entityManager.merge(burger);
     }
-
+    @Transactional
     @Override
     public Burger delete(Long id) {
-        return null;
+        Burger burger = findById(id);
+        entityManager.remove(burger);
+        return burger;
     }
+
+    //TODO 1.19.00 video devam
 }
